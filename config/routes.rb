3f65b74687e20f29b8c27c1eaf0dd021c075f64c
Winicius Reis <winireis@gmail.com>
2019-05-06 09:44:41 -0300

    API signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/signup', to: 'users#create'
+  post '/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
 end
